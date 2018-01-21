d42c9cc406f4706ee6d3708cbcd164ee6f06724a
benfang2002 <benfang2002@163.com>
2018-01-21 21:20:28 +0800

    fix

--- complete/src/main/java/hello/Validation/PasswordMatchesValidator.java
+++ complete/src/main/java/hello/Validation/PasswordMatchesValidator.java
@@ -14,7 +14,7 @@ implements ConstraintValidator<PasswordMatches, Object> {
   @Override
   public boolean isValid(Object obj, ConstraintValidatorContext context){   
       UserDto user = (UserDto) obj;
-      return user.getpassword().equals(user.getMatchingpassword());    
+      return user.getpassword().equals(user.getmatchingpassword());    
   }
      
 }
